704a142dbab9818bc462de1e9b39d45d2e030738
nuratdinovsh <sh.nuratdinov@gmail.com>
2026-08-09 17:16:03 +0500

    changed install.sh

--- config/install.sh
+++ config/install.sh
@@ -1,11 +1,32 @@
 #!/usr/bin/env bash
 set -e
 
-sudo pacman -S --needed --noconfirm hyprland hyprpaper alacritty nvim wl-clipboard ttf-jetbrains-mono-nerd inter-font
+sudo pacman -S --needed --noconfirm \
+    hyprland \
+    hyprpaper \
+    alacritty \
+    neovim \
+    wl-clipboard \
+    ttf-jetbrains-mono-nerd \
+    inter-font \
 
-mv ~/dotfiles/config/hypr ~/.config
-mv ~/dotfiles/config/.bashrc ~/
+if ! command -v yay &> /dev/null; then
+    echo "Installing yay..."
+
+    git clone https://aur.archlinux.org/yay.git /tmp/yay
+    cd /tmp/yay
+    makepkg -si --noconfirm
+    cd -
+    rm -rf /tmp/yay
+fi
+
+echo "Installing Google Chrome..."
+yay -S --needed --noconfirm google-chrome
+
+mkdir -p ~/.config
+
+cp -r ~/dotfiles/config/hypr ~/.config/
+cp ~/dotfiles/config/.bashrc ~/.bashrc
 source ~/.bashrc
 
 echo "Process finished"
-
