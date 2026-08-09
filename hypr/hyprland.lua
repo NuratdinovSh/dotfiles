5eb8ea13b7118d18fc2ffbcc382cc5eba1e1e26e
nuratdinovsh <sh.nuratdinov@gmail.com>
2026-08-09 16:36:09 +0500

    add hyprland config

---
+++ hypr/hyprland.lua
@@ -0,0 +1,10 @@
+require("monitors")
+require("decorations")
+require("animations")
+require("misc")
+require("keybinds")
+
+
+hl.on("hyprland.start", function () 
+  hl.exec_cmd("hyprpaper &")
+end)
