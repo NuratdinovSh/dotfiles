902e91a098c455fbfb5a352a97813615d121a908
nuratdinovsh <sh.nuratdinov@gmail.com>
2026-08-09 16:56:51 +0500

    added install.sh and .bashrc

---
+++ config/install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e
+
+sudo pacman -S --needed --noconfirm hyprland hyprpaper alacritty nvim wl-clipboard ttf-jetbrains-mono-nerd inter-font
+
+mv ~/dotfiles/config/hypr ~/.config/hypr
+mv ~/dotfiles/config/.bashrc ~/
+source ~/.bashrc
+
+echo "Process finished"
+
