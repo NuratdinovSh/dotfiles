c4289be52835caa4d51c9cc4556646d88119d436
nuratdinovsh <sh.nuratdinov@gmail.com>
2026-08-09 18:26:34 +0500

    changed alacritty and neovim

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -13,60 +13,38 @@ vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
 vim.opt.expandtab = true
 
-local colors = {
-    bg       = "#101114",
-    fg       = "#FEFCFD",
-
-    black    = "#17181C",
-    red      = "#e8998d",
-    green    = "#52B788",
-    yellow   = "#f4d58d",
-    blue     = "#83C5BE",
-    magenta  = "#EA9AB2",
-    cyan     = "#56b6c2",
-    white    = "#D8DEE9",
-
-    cursor   = "#BFCDE0",
-    selection = "#30343F",
-}
-
 vim.api.nvim_set_hl(0, "Normal", {
-    fg = colors.fg,
+    fg = "#eeeeee",
     bg = "NONE",
 })
 
-vim.api.nvim_set_hl(0, "Comment", {
-    fg = colors.blue,
-})
-
 vim.api.nvim_set_hl(0, "String", {
-    fg = colors.green,
+    fg = "#8fb996",
 })
 
 vim.api.nvim_set_hl(0, "Number", {
-    fg = colors.yellow,
+    fg = "#aed9e0",
 })
 
 vim.api.nvim_set_hl(0, "Function", {
-    fg = colors.cyan,
+    fg = "#a3c4f3",
 })
 
 vim.api.nvim_set_hl(0, "Keyword", {
-    fg = colors.magenta,
+    fg = "#faedcd",
 })
 
 vim.api.nvim_set_hl(0, "Type", {
-    fg = colors.blue,
+    fg = "#bde0fe",
 })
 
 vim.api.nvim_set_hl(0, "Constant", {
-    fg = colors.red,
+    fg = "#ffb5a7",
 })
 
 vim.api.nvim_set_hl(0, "LineNr", {
-    fg = colors.blue,
+    fg = "#6d6875",
 })
-
-vim.api.nvim_set_hl(0, "CursorLineNr", {
-    fg = colors.fg,
+vim.api.nvim_set_hl(0, "Identifier", {
+    fg = "#cdb4db",
 })
