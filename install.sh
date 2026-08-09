41be66e547229c1b38aa7fd84022f97b2ead2d28
nuratdinovsh <sh.nuratdinov@gmail.com>
2026-08-09 18:59:51 +0500

    added themes

--- install.sh
+++ install.sh
@@ -9,14 +9,16 @@ sudo pacman -S --needed --noconfirm \
     wl-clipboard \
     ttf-jetbrains-mono-nerd \
     inter-font \
+    glib2 \
 
 if ! command -v yay &> /dev/null; then
     echo "Installing yay..."
 
     git clone https://aur.archlinux.org/yay.git /tmp/yay
-    cd /tmp/yay
-    makepkg -si --noconfirm
-    cd -
+    (
+        cd /tmp/yay
+        makepkg -si --noconfirm
+    )
     rm -rf /tmp/yay
 fi
 
@@ -24,9 +26,18 @@ echo "Installing Google Chrome..."
 yay -S --needed --noconfirm google-chrome
 
 mkdir -p ~/.config
+mkdir -p ~/.local/share/themes
+mkdir -p ~/.local/share/icons
 
 cp -r ~/dotfiles/config/hypr ~/.config/
+cp -r ~/dotfiles/config/alacritty ~/.config/
+cp -r ~/dotfiles/config/nvim ~/.config/
 cp ~/dotfiles/config/.bashrc ~/.bashrc
-source ~/.bashrc
+cp -r ~/dotfiles/config/Colloid-Dark ~/.local/share/themes/
+cp -r ~/dotfiles/config/Papirus-Dark ~/.local/share/icons/
+
+gsettings set org.gnome.desktop.interface gtk-theme "Colloid-Dark"
+gsettings set org.gnome.desktop.interface icon-theme "Papirus-Dark"
+gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
 
 echo "Process finished"
