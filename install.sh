0937fed5fc75317e31cd5f5cb9721de312c6e766
nuratdinovsh <sh.nuratdinov@gmail.com>
2026-08-09 23:53:41 +0500

    test

--- install.sh
+++ install.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-# dotfiles repo is wherever this script itself lives (you already cloned it
-# to get this file, so no need to clone it again)
-DOTFILES_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
 # --- base tools needed before anything else (git assumed already present) --
 sudo pacman -S --needed --noconfirm \
     base-devel \
@@ -45,13 +41,13 @@ sudo mkdir -p /etc/sddm.conf.d
 printf '[Theme]\nCurrent=sddm-theme\n' | sudo tee /etc/sddm.conf.d/theme.conf
 
 # --- copy configs from dotfiles ------------------------------------------
-cp -r "$DOTFILES_DIR/config/hypr" ~/.config/
-cp -r "$DOTFILES_DIR/config/alacritty" ~/.config/
-cp -r "$DOTFILES_DIR/config/nvim" ~/.config/
-cp "$DOTFILES_DIR/config/.bashrc" ~/.bashrc
-cp -r "$DOTFILES_DIR/config/Colloid-Dark" ~/.local/share/themes/
-cp -r "$DOTFILES_DIR/config/Papirus-Dark" ~/.local/share/icons/
-sudo cp -r "$DOTFILES_DIR/config/sddm-theme" /usr/share/sddm/themes/
+cp -r ~/dotfiles/config/hypr/ ~/.config/
+cp -r ~/dotfiles/config/alacritty/ ~/.config/
+cp -r ~/dotfiles/config/nvim/ ~/.config/
+cp ~dotfiles/config/.bashrc/ ~/.bashrc
+cp -r ~/dotfiles/config/Colloid-Dark ~/.local/share/themes/
+cp -r ~/dotfiles/config/Papirus-Dark ~/.local/share/icons/
+sudo cp -r ~/dotfiles/config/sddm-theme /usr/share/sddm/themes/
 
 # --- gtk theming (dconf now installed, so gsettings works) ---------------
 gsettings set org.gnome.desktop.interface gtk-theme "Colloid-Dark"
